d9b3c57b4dc0c7954d42e092d2a52dcc9b0f9d0d
Chriss <ccallef@udd.cl>
2025-12-03 01:10:33 +0100

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import RobotMonitor from './features/RobotMonitor/RobotMonitor';
-
+import StatsPage from './features/StatsMonitor/StatsPage';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<RobotMonitor />} />
+      <Route path="/stats" element={<StatsPage />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Routes>
   );
