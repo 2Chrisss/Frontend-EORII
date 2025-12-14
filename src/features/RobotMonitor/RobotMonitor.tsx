e7e616f4c17823d6e156a89f796cb496b0768ad2
Chriss <ccallef@udd.cl>
2025-12-14 23:14:12 +0100

    Update RobotMonitor.tsx

--- src/features/RobotMonitor/RobotMonitor.tsx
+++ src/features/RobotMonitor/RobotMonitor.tsx
@@ -351,7 +351,6 @@ const RobotMonitor: React.FC = () => {
                     {/* Encabezado del robot */}
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 8 }}>
                       <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-                        <span style={{ fontSize: 18 }}></span>
                         <span style={{ fontWeight: 700, fontSize: 14 }}>Robot {robot.id}</span>
                       </div>
                       <span 
